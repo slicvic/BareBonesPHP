03b349402279eefff08c93344510bb87cff84197
victor lantigua <slicvic@victors-MacBook-Pro.local>
2014-07-30 12:31:30 -0400

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -5,17 +5,17 @@ function __autoload($class)
     $class = str_replace('_', '/', $class);
 
     $paths = array(
-    	APPPATH.'classes/'.$class.'.php',
-    	LIBPATH.$class.'.php'
-	);
+        APPPATH.'classes/'.$class.'.php',
+        LIBPATH.$class.'.php'
+    );
 
-	foreach($paths as $path)
-	{
-		if (file_exists($path)) {
-			require $path;
-			return TRUE;
-		}
-	}
+    foreach($paths as $path)
+    {
+        if (file_exists($path)) {
+            require $path;
+            return TRUE;
+        }
+    }
 
     throw new Candy_Exception('The requested '.$path.' could not be found', 500);
 }
